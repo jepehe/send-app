f816a546c7cf5066c6c1fd7ad40b6234500d03eb
kg <jepehe@163.com>
2015-05-26 14:32:49 +0800

    #version1#

---
+++ js/index.js
@@ -0,0 +1,12 @@
+
+$(function(){
+  $(".an1").addClass('an1-t');
+  $(".an2").addClass('an2-t');
+  $(".an3").addClass('an3-t');
+  $(".an6").addClass('an6-t');
+  $(".an4").addClass('an4-t');
+  $(".an5").addClass('an5-t');
+  $(".an7").addClass('an7-t');
+  $(".an8").addClass('an8-t');
+  $(".an9").addClass('an9-t');
+})
